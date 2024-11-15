1f69ba11d7cbf68942a475f2c3cdab02a98ee59b
Zachar-gifka <zahark628@gmail.com>
2024-11-15 04:38:24 +0300

    улучшение кода по Code Review

--- project/src/index.js
+++ project/src/index.js
@@ -3,10 +3,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Используем новый API для React 18
 import App from './App';  // Импортируем главный компонент приложения
-import './styles/styles.css';  // Убедитесь, что путь правильный
+import './styles/styles.css';  
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-    // Стилевой файл, если есть
+    
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Создаем корень
 root.render(
